45f5ca2711620131904fdbc9a86167011552a16f
loganyeh <yehlogan@gmail.com>
2025-11-11 18:26:13 -0500

    feat: simple next and back page logic logs

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+const nextButton = document.getElementById("next-button");
+const backButton = document.getElementById("back-button");
+let onPage = 1;
+const firstPageNumber = 1;
+const lastPageNumber = 4;
+console.log(`You are on Page 1`);
+
+nextButton.addEventListener("click", () => {
+    if(onPage < lastPageNumber){
+        onPage++;
+        console.log(`Page: ${onPage}. Next button clicked.`);
+    }
+    else {
+        console.log(`You are on the last page. ${onPage}`);
+    }
+
+});
+
+backButton.addEventListener("click", () => {
+    if(onPage > firstPageNumber){
+        onPage--;
+        console.log(`Page: ${onPage}. back button clicked`);
+    }
+    else{
+        console.log(`You are on the first page. ${onPage}`);
+    }
+
+});
+
+// NEXT PAGE FUNCTION
+function nextPage(){
+    const pages = ["page1", "page2", "page3", "page4"];
+
+    pages.forEach((page) => {
+        const el = document.getElementById(`page${page}`)
+    });
+
+}
+
+// PREVIOUS PAGE FUNCTION
+function previousPage(){
+
+}
